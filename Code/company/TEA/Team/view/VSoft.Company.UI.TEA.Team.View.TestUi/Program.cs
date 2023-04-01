cb6733fec39ec98e06e7e3096941ec78f5a2fe9a
Phat <nguyentanphat8694@gmail.com>
2023-04-01 15:16:26 +0700

    u

--- Code/company/TEA/Team/view/VSoft.Company.UI.TEA.Team.View.TestUi/Program.cs
+++ Code/company/TEA/Team/view/VSoft.Company.UI.TEA.Team.View.TestUi/Program.cs
@@ -1,7 +1,3 @@
-using Blazored.Toast;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using VegunSoft.Framework.Api.DtoClient.Token.Provider.Services;
 using VSoft.Common.UI.APP.Base.View.Main;
 using VSoft.Company.UI.TEA.Team.View.Module;
 using VSoft.Company.UI.TEA.Team.View.TestUi;
