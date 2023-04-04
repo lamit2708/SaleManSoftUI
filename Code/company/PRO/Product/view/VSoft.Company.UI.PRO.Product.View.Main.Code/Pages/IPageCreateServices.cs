ba6f8450858057a8a0e894c7fca60d74d835b774
nguyensonghuong <nguyen.songhuongit@gmail.com>
2023-04-04 19:14:58 +0700

    CREATE PRODUCT UI

---
+++ Code/company/PRO/Product/view/VSoft.Company.UI.PRO.Product.View.Main.Code/Pages/IPageCreateServices.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace VSoft.Company.UI.PRO.Product.View.Main.Code.Pages
+{
+    public interface IPageCreateServices
+    {
+        List<string> PageMessage { get; }
+        bool IsSuccess { get; }
+        Task CreateProducts(string name, string description);
+    }
+}
